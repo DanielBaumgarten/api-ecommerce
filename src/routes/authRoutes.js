0c124f8c5ded2028cee66f15fca818b5275f0a35
DanielBaumgarten <86086435+DanielBaumgarten@users.noreply.github.com>
2026-07-04 19:57:02 -0300

    feat: documenta rotas do Auth

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,13 +2,47 @@ const express = require("express");
 
 const router = express.Router();
 
-const authController =
-  require("../controllers/authController");
+const authController = require("../controllers/authController");
+
+/**
+ * @swagger
+ * /auth/register:
+ *   post:
+ *     summary: Cadastro de usuário
+ *     tags: [Autenticação]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               nome:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               senha:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: Usuário cadastrado
+ */
 
 router.post(
   "/register",
   authController.cadastrar
 );
+
+/**
+ * @swagger
+ * /auth/login:
+ *   post:
+ *     summary: Login do usuário
+ *     tags: [Autenticação]
+ *     responses:
+ *       200:
+ *         description: Token JWT gerado
+ */
 
 router.post(
   "/login",
